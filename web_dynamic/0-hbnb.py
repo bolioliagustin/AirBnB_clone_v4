ba2e6a3b1364402f3e77a32fc6990bc2d79bfbff
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-02-11 13:16:39 -0300

    Uuid

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -6,7 +6,9 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
+import uuid
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
