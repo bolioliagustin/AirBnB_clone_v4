a39b726b1471167049432fa8cb8f53d7d8098178
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-02-11 14:51:59 -0300

    s

--- web_dynamic/scripts/1-hbnb.js
+++ web_dynamic/scripts/1-hbnb.js
@@ -1,1 +1,11 @@
-s
+document.addEventListener("DOMContentLoaded", ready);
+$( function () {
+    $(checkbox).prop(true)( function () {
+        var id = amenity_id;
+    })
+    $(checkbox).prop(false)( function () {
+        var id = none
+    }
+    
+    )
+})
